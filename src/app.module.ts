b2be3e1373dae37cd4f63d18f4837b86642d0c79
Timo Wang <timowang1991@gmail.com>
2022-04-10 18:15:35 +0800

    products CRUD all ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 // decorator to attach to a class
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [AppController], // handle requests
   providers: [AppService], // extra services (e.g. DB client) to inject to controllers
 })
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
-  products: Product[] = [];
+  private products: Product[] = [];
 
-  insertProduct(title: string, desc: string, price: number): void {
-    const newProduct = new Product(new Date().toString(), title, desc, price);
+  insertProduct(title: string, desc: string, price: number) {
+    const prodId = Math.random().toString();
+    const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
+    return prodId;
+  }
+
+  getProducts() {
+    return [...this.products];
+  }
+
+  getSingleProduct(productId: string) {
+    const [product] = this.findProduct(productId);
+    return {...product};
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    this.products[index] = {
+      ...product,
+      title: title || product.title,
+      description: desc || product.description,
+      price: price || product.price,
+    }
+  }
+
+  deleteProduct(productId: string) {
+    const [product, index] = this.findProduct(productId);
+    this.products.splice(index, 1);
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIdx = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIdx];
+    if (!product) {
+      throw new NotFoundException('Could not find product');
+    }
+    return [product, productIdx];
   }
 }
